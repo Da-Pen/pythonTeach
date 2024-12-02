2ab4b885ff58fa966e31a350214dd533aea302c4
Daniel Peng <peng.daniel11440@gmail.com>
2024-12-02 18:41:31 -0500

    refactor out nested loops to new file

---
+++ contest/nested_loops.py
@@ -0,0 +1,39 @@
+
+# Exercise: Write a function that, given a number n, prints the following with n rows:
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# Exercise: Write a function that prints a 10x10 multiplication table
+# Solution:
+def print_multiplication_table():
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(i * j, end="\t")
+        print()
+
+# Exercise: Write a function that returns the first n rows of Pascal's Triangle.
+# Pascal's Triangle looks like this:
+#
+#        1
+#       1 1
+#      1 2 1
+#     1 3 3 1
+#    1 4 6 4 1
+#  1 5 10 10 5 1
+# 1 6 15 20 15 6 1
+#       ...
+#
+# Solution:
+def pascals_triangle(n):
+    l = [[1]]
+    for i in range(1, n):
+        new_row = [1] * (i+1)
+        for j in range(1, i):
+            new_row[j] = l[i - 1][j - 1] + l[i - 1][j]
+        l.append(new_row)
+    return l
+
+print(pascals_triangle(10))
